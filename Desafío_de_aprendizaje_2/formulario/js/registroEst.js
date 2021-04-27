c3bdfea34e2f0a2ba9d7cb132b7ade6de113027e
kevinsanchezcano08 <61790908+kevinsanchezcano08@users.noreply.github.com>
2021-04-26 21:36:21 -0600

    Add files via upload

---
+++ Desafío_de_aprendizaje_2/formulario/js/registroEst.js
@@ -0,0 +1,12 @@
+function registro(){
+    nombre= document.getElementById('name').value;
+    fechaNac = document.getElementById('dateBirth').value;
+    numDUI = document.getElementById('DUI').value;
+    genero = document.getElementById('gender').value;
+    correo = document.getElementById('email').value;
+    numcel = document.getElementById('cel').value;
+    direc = document.getElementById('address').value;
+    
+    alert("¡Estudiante registrado!");
+    
+}
